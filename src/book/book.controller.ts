220e709414025c4ddc63c660361fcdcc580079de
Jeonghwan Park <pjhisgod@naver.com>
2023-11-09 22:47:18 +0900

    feat : bookservice findall

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,4 +1,14 @@
+import { Get } from '@nestjs/common';
+import { BookService } from './book.service';
 import { Controller } from '@nestjs/common';
+import { Book } from 'src/model/entity/Book.entity';
 
 @Controller('book')
-export class BookController {}
+export class BookController {
+  constructor(private readonly bookService: BookService) {} //BookService  주입
+
+  @Get()
+  getAll(): Promise<Book[]> {
+    return this.bookService.findAll();
+  }
+}
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Book } from 'src/model/entity/Book.entity';
 
 @Injectable()
-export class BookService {}
+export class BookService {
+  constructor(
+    @InjectRepository(Book) private readonly bookRepository: Repository<Book>,
+  ) {} //Book repository inject
+
+  findAll(): Promise<Book[]> {
+    return this.bookRepository.find();
+  }
+}
