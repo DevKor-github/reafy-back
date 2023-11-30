98eaca7e89761f2a7e951eaf1e12a3662651cdcb
Jeonghwan Park <pjhisgod@naver.com>
2023-11-30 22:08:21 +0900

    feat : bookshelfbookdto

--- src/book/dto/BookshelfBookDetail.dto.ts
+++ src/book/dto/BookshelfBookDetail.dto.ts
@@ -1,18 +1,46 @@
+import { IsNumber, IsString } from 'class-validator';
+
 export class BookshelfBookDetailDto {
+  @IsNumber()
   bookshelfbookId: number;
+
+  @IsNumber()
   userId: number;
+
+  @IsNumber()
   progressState: number;
+
+  @IsNumber()
   bookId: number;
+
+  @IsString()
   title: string;
+
+  @IsString()
   author: string;
+
+  @IsString()
   content: string;
+
+  @IsString()
   publisher: string;
+
+  @IsString()
   thumbnailURL: string;
+
+  @IsString()
   link: string;
+
+  @IsString()
   category: string;
 
+  @IsNumber()
   pages: number;
+
+  @IsNumber()
   startPage: number;
+
+  @IsNumber()
   endPage: number;
 
   static async makeRes(data, startPage, endPage) {
