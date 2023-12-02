6db18ef40239db186e0592298ecc91b69e7d6b7a
jeongmin hwang <jeongmin010324@gmail.com>
2023-12-02 22:38:03 +0900

    feat : login swagger 추가

--- src/model/entity/User.entity.ts
+++ src/model/entity/User.entity.ts
@@ -1,10 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('user')
 export class User {
+  
+  @ApiProperty({ description: '유저 id' })
   @PrimaryGeneratedColumn({ name: 'user_id' })
   userId: number;
 
+  @ApiProperty({ description: 'oauth user id' })
   @Column({ name: "oauth_id" })
   oauthId: string;
 
