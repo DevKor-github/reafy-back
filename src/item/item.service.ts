a9989f5596b924b3b778844c12b43c7fb3a963bb
jeongmin hwang <jeongmin010324@gmail.com>
2023-12-28 17:50:55 +0900

    feat : item 구매 시 coin 감소

--- src/item/item.service.ts
+++ src/item/item.service.ts
@@ -3,11 +3,14 @@ import { UserItemRepository } from './userItem.repository';
 import { UserItem } from 'src/model/entity/UserItem.entity';
 import { Repository } from 'typeorm';
 import { ItemDto } from './dto/item.dto';
+import { ItemReqDto } from './dto/itemReq.dto';
+import { CoinService } from 'src/coin/coin.service';
 
 @Injectable()
 export class ItemService {
     constructor(
         private readonly userItemRepository: UserItemRepository,
+        private readonly coinService: CoinService,
 
     ) { }
 
@@ -25,15 +28,16 @@ export class ItemService {
 
     }
 
-    async buyItem(userId: number, itemDto: ItemDto): Promise<ItemDto> {
-        let requestItem: UserItem = this.toUserItemEntity(itemDto, userId); // todo activation ture of false ?
+    async buyItem(userId: number, itemReqDto: ItemReqDto): Promise<ItemDto> {
+        let requestItem: UserItem = this.toUserItemEntity(itemReqDto, userId); // todo activation ture of false ?
 
-        let item: UserItem = await this.userItemRepository.getUserItem(userId, itemDto.itemId);
+        let item: UserItem = await this.userItemRepository.getUserItem(userId, itemReqDto.itemId);
         if (item) {
-            item.activation = itemDto.activation;
+            item.activation = itemReqDto.activation;
             requestItem = item;
         }
 
+        await this.coinService.useCoin(userId, itemReqDto.price);
         const savedItem = await this.userItemRepository.create(requestItem);
         return new ItemDto().setDataByUserItemEntity(savedItem);
     }
