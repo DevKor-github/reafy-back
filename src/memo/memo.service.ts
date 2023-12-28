0bcafe709d32aa1698c266d869251be2e4d02367
Jeonghwan Park <pjhisgod@naver.com>
2023-12-28 15:03:19 +0900

    fix : search memo via hashtag

--- src/memo/memo.service.ts
+++ src/memo/memo.service.ts
@@ -1,4 +1,4 @@
-import { Hashtag } from 'src/model/entity/Hashtag.entity';
+import { Hashtag } from './../model/entity/Hashtag.entity';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Memo } from 'src/model/entity/Memo.entity';
@@ -29,16 +29,7 @@ export class MemoService {
 
     await Promise.all(
       memoList.map(async (memo) => {
-        const hashtags = [];
-        const hashtagData = await this.memoHashtagRepository.query(
-          `
-        SELECT hashtag.keyword
-        FROM memo_hashtag
-        LEFT JOIN hashtag on memo_hashtag.hashtag_id = hashtag.hashtag_id
-        WHERE memo_hashtag.memo_id = ${memo.memoId}
-        `,
-        );
-        hashtagData.map((hashtag) => hashtags.push(hashtag.keyword));
+        const hashtags = await this.getHashtagsByMemoId(memo.memoId);
         resultArray.push(await MemoResDto.makeRes(memo, hashtags));
       }),
     );
@@ -60,35 +51,40 @@ export class MemoService {
     console.log(offset);
     const memoList = await this.memoHashtagRepository.query(
       `
-      SELECT *
-      FROM memo_hashtag
-      LEFT JOIN memo ON memo_hashtag.memo_id = memo.memo_id
-      WHERE memo.user_id = ${userId} AND memo_hashtag.hashtag_id = ${selectedHashtag.hashtagId}
+      SELECT sq.memo_hashtag_id, memo.user_id, memo.memo_id, memo.bookshelf_book_id, memo.content, memo.page, memo.imageURL, memo.created_at, memo.updated_at
+      FROM (SELECT * FROM(SELECT * FROM memo_hashtag GROUP BY hashtag_id, memo_id) AS subquery WHERE deleted_at IS NULL) as sq
+      LEFT JOIN memo ON sq.memo_id = memo.memo_id
+      WHERE memo.user_id = ${userId} AND sq.hashtag_id = ${selectedHashtag.hashtagId} AND memo.deleted_at IS NULL
       ORDER BY memo.created_at DESC
       LIMIT 10 OFFSET ${offset};
       `,
     );
 
+    /* 
+      SELECT memo_hashtag_id, memo_hashtag.memo_id, memo_hashtag.hashtag_id, memo.created_at, memo_hashtag.deleted_at
+      FROM (SELECT * FROM memo_hashtag GROUP BY memo_hashtag.hashtag_id, memo_hashtag.memo_id) AS subquery
+      LEFT JOIN memo ON memo_hashtag.memo_id = memo.memo_id 
+      WHERE memo.user_id = 6 AND memo_hashtag.hashtag_id = 7 AND memo_hashtag.deleted_at IS NULL
+      ORDER BY memo.created_at DESC
+      LIMIT 10 OFFSET 0;
+
+      SELECT sq.memo_hashtag_id, memo.user_id, memo.memo_id, memo.bookshelf_book_id, memo.content, memo.page, memo.imageURL, memo.created_at, memo.updated_at
+      FROM (SELECT * FROM(SELECT * FROM memo_hashtag GROUP BY hashtag_id, memo_id) AS subquery WHERE deleted_at IS NULL) as sq
+      LEFT JOIN memo ON sq.memo_id = memo.memo_id
+      WHERE memo.user_id = 6 AND sq.hashtag_id = 9
+      ORDER BY memo.created_at DESC
+      LIMIT 10 OFFSET 0; -> 이게 진짜임
+      
+      (SELECT * FROM(SELECT * FROM memo_hashtag GROUP BY hashtag_id, memo_id) AS subquery WHERE deleted_at IS NULL) */
+
     await Promise.all(
       memoList.map(async (memo) => {
-        const hashtags = [];
-        console.log(memo);
-        const hashtagData = await this.memoHashtagRepository.query(
-          `
-        SELECT hashtag.keyword
-        FROM memo_hashtag
-        LEFT JOIN hashtag on memo_hashtag.hashtag_id = hashtag.hashtag_id
-        WHERE memo_hashtag.memo_id = ${memo.memo_id};
-        `,
-        );
-
         memo.userId = memo.user_id;
         memo.bookshelfBookId = memo.bookshelf_book_id;
         memo.memoId = memo.memo_id;
         memo.createdAt = memo.created_at;
         memo.updatedAt = memo.updated_at;
-
-        hashtagData.map((hashtag) => hashtags.push(hashtag.keyword));
+        const hashtags = await this.getHashtagsByMemoId(memo.memo_id);
         resultArray.push(await MemoResDto.makeRes(memo, hashtags));
       }),
     );
@@ -114,27 +110,35 @@ export class MemoService {
 
     await Promise.all(
       memoList.map(async (memo) => {
-        const hashtags = [];
-        const hashtagData = await this.memoHashtagRepository.query(
-          `
-        SELECT hashtag.keyword
-        FROM memo_hashtag
-        LEFT JOIN hashtag on memo_hashtag.hashtag_id = hashtag.hashtag_id
-        WHERE memo_hashtag.memo_id = ${memo.memo_id};
-        `,
-        );
         memo.userId = memo.user_id;
         memo.bookshelfBookId = memo.bookshelf_book_id;
         memo.memoId = memo.memo_id;
         memo.createdAt = memo.created_at;
         memo.updatedAt = memo.updated_at;
 
-        hashtagData.map((hashtag) => hashtags.push(hashtag.keyword));
+        const hashtags = await this.getHashtagsByMemoId(memo.memo_id);
+
         resultArray.push(await MemoResDto.makeRes(memo, hashtags));
       }),
     );
 
     return resultArray.sort((a, b) => b.createdAt - a.createdAt);
+  }
+
+  async getHashtagsByMemoId(memoId: number) {
+    const hashtags = [];
+    const hashtagData = await this.memoHashtagRepository.query(
+      `
+    SELECT hashtag.keyword
+    FROM memo_hashtag
+    LEFT JOIN hashtag on memo_hashtag.hashtag_id = hashtag.hashtag_id
+    WHERE memo_hashtag.memo_id = ${memoId} AND memo_hashtag.deleted_at IS NULL;
+    `,
+    );
+
+    hashtagData.map((hashtag) => hashtags.push(hashtag.keyword));
+
+    return hashtags;
   }
 
   async getMemoDetail(userId: number, memoId: number) {
@@ -148,7 +152,9 @@ export class MemoService {
       },
     });
 
-    return memo;
+    const hashtags = await this.getHashtagsByMemoId(memoId);
+
+    return await MemoResDto.makeRes(memo, hashtags);
   }
 
   async createMemo(
