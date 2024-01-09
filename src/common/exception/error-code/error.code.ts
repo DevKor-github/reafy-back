d7fb53b8b1d52212062a8b01623bcc7d5b1bdb57
Jeonghwan Park <pjhisgod@naver.com>
2024-01-09 14:52:50 +0900

    feat : Auth, User, Coin ErrorCode/Exceptions

--- src/common/exception/error-code/error.code.ts
+++ src/common/exception/error-code/error.code.ts
@@ -15,6 +15,20 @@ export const INTERNAL_SERVER_ERROR = new ErrorCodeObject(
   'Internal Server Error',
 );
 //AuthService ErrorCode
+export const VENDOR_NOT_EXIST = new ErrorCodeObject(400, 'Vendor Not Exist');
+
+export const BAD_ACCESS_TOKEN = new ErrorCodeObject(400, 'Bad Access Token');
+
+export const INVALID_REFRESH_TOKEN = new ErrorCodeObject(
+  401,
+  'Invalid Refresh Token',
+);
+
+export const UNAUTHORIZED_USER = new ErrorCodeObject(401, 'Unauthorized User');
+
+//UserService ErrorCode
+export const USER_NOT_FOUND = new ErrorCodeObject(404, 'User Not FOUND');
+
 //BookService ErrorCode
 export const API_ACCESS_ERROR = new ErrorCodeObject(500, 'API access Error');
 export const INVALID_LOGIN_ACCESS = new ErrorCodeObject(
@@ -23,6 +37,7 @@ export const INVALID_LOGIN_ACCESS = new ErrorCodeObject(
 );
 
 export const INVALID_ISBN = new ErrorCodeObject(400, 'Invalid ISBN');
+
 export const BOOK_NOT_FOUND = new ErrorCodeObject(
   404,
   'Requested BookshelfBook is NOT FOUND ',
@@ -48,3 +63,10 @@ export const HISTORY_NOT_FOUND = new ErrorCodeObject(
   404,
   'Requested History is NOT FOUND',
 );
+
+//CoinService ErrorCode
+export const NOT_ENOUGH_COIN = new ErrorCodeObject(400, 'Not Enought Coin');
+
+//ItemService ErrorCode - none
+
+//StatisticsService ErrorCode - none
