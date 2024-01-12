eda1e276db393561fed8b2e4b94732012db37a20
jeongmin hwang <jeongmin010324@gmail.com>
2024-01-13 02:37:41 +0900

    fix : 인증 서비스 미사용 import 제거 code 수정

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -1,16 +1,15 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { CoinModule } from 'src/coin/coin.module';
+import { ACCESS_TOKEN_EXPIRE } from 'src/common/constant/authentication.constant';
+import { UserModule } from 'src/user/user.module';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
-import { UserModule } from 'src/user/user.module';
-import { HttpModule } from '@nestjs/axios';
-import { JwtModule } from '@nestjs/jwt';
-import { ACCESS_TOKEN_EXPIRE } from 'src/common/constant/authentication.constant';
 import { JwtAccessStrategy } from './jwt-access.strategy';
 import { JwtRefreshStrategy } from './jwt-refresh.strategy';
-import { PassportModule } from '@nestjs/passport';
-import { ConfigService } from '@nestjs/config';
-import { CoinService } from 'src/coin/coin.service';
-import { CoinModule } from 'src/coin/coin.module';
 
 @Module({
   imports: [
