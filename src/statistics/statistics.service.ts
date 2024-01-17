cb53757b8e66872e506d7d250cee1ece3f6fdf12
jeongmin hwang <jeongmin010324@gmail.com>
2024-01-17 15:35:14 +0900

    fix : 통계 로깅 추가

--- src/statistics/statistics.service.ts
+++ src/statistics/statistics.service.ts
@@ -1,15 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, LoggerService } from '@nestjs/common';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { UserBookHistoryRepository } from 'src/history/repository/user-book-history.repository';
 import {
   MonthlyTotalPagesListDto,
   MonthlyTotalReadingTimesListDto,
 } from './dtos/Statistics.dto';
-import { UserBookHistoryRepository } from 'src/history/repository/user-book-history.repository';
 
 @Injectable()
 export class StatisticsService {
   constructor(
     private readonly userBookHistoryRepository: UserBookHistoryRepository,
-  ) {}
+    @Inject(WINSTON_MODULE_NEST_PROVIDER) private readonly logger: LoggerService
+  ) { }
 
   async getMonthlyTotalPages(
     userId: number,
@@ -21,13 +23,11 @@ export class StatisticsService {
 
     //독서 기록 유무 상관없이 그 해의 모든 총 페이지 수 반환. 없으면 0으로 뜰 것
 
-    await Promise.all(
-      resultArray.map((resultPacket) => {
-        monthlyTotalPagesList.push(
-          MonthlyTotalPagesListDto.makeRes(resultPacket),
-        );
-      }),
-    );
+    resultArray.map((resultPacket) => {
+      monthlyTotalPagesList.push(
+        MonthlyTotalPagesListDto.makeRes(resultPacket),
+      );
+    });
 
     return monthlyTotalPagesList;
   }
@@ -44,14 +44,12 @@ export class StatisticsService {
       );
 
     //독서 기록 유무 상관없이 그 해의 모든 총 독서 시간 반환. 없으면 0으로 뜰 것
-    await Promise.all(
+    
       resultArray.map((resultPacket) => {
         monthlyTotalReadingTimesList.push(
           MonthlyTotalReadingTimesListDto.makeRes(resultPacket),
         );
-      }),
-    );
-
+      });
     return monthlyTotalReadingTimesList;
   }
 }
