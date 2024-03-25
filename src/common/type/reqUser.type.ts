8fabb163661252dafe1cd6d4fd109c6b0a052d64
Jeonghwan Park <pjhisgod@naver.com>
2024-03-25 17:08:37 +0900

    feat : storage migration to S3

--- src/common/type/reqUser.type.ts
+++
@@ -1,6 +0,0 @@
-import { AccessTokenPayload } from 'src/authentication/dto/AccessToken.payload';
-declare global {
-  namespace Express {
-    export interface User extends AccessTokenPayload {}
-  }
-}
